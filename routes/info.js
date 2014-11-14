7edf5a094474774ee725f8eb519e415b3d2a1622
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-14 23:36:42 +0200

    -- change tha way info bar loads without callbacks

--- routes/info.js
+++ routes/info.js
@@ -10,33 +10,36 @@ router.get('/:getValues', function(req, res) {
         query = "SELECT nextDraw from next_draw";
         connection.query(query ,function(err,nextDrawRow)     {
         if(err)
-            res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
+//            res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
+			console.log("Error Selecting next draw: %s " + err );
             result.nextDraw = nextDrawRow[0].nextDraw;
-			//get last draw
-			query = "SELECT * from draw order by drawDateTime DESC LIMIT 1";
-			connection.query(query ,function(err,lastDrawRow)     {
-			if(err)
-				res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
-				result.lastDraw = lastDrawRow[0];
-				//get user info if user is logged in
-				if (req.session.user){
-					query = "SELECT * from users where userId = " + req.session.user.userId;
-					connection.query(query ,function(err,userRow)     {
-					if(err)
-						res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
-						req.session.user = userRow[0];
-						result.userCoins = userRow[0].userCoins;
-//						console.log(result);
-						res.send(result);
-					});
-				} else {
-					result.userCoins = 0;
-					res.send(result);
-				}
-			});
-
         });
-    });
+
+		//get user info if user is logged in
+		if (req.session.user){
+			query = "SELECT * from users where userId = " + req.session.user.userId;
+			connection.query(query ,function(err,userRow)     {
+			if(err)
+//				res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
+				console.log("Error Selecting user information: %s " + err );
+				req.session.user = userRow[0];
+				result.userCoins = userRow[0].userCoins;
+			});
+		} else {
+			result.userCoins = 0;
+		}
+
+		//get last draw
+		query = "SELECT * from draw order by drawDateTime DESC LIMIT 1";
+		connection.query(query ,function(err,lastDrawRow)     {
+		if(err)
+//			res.send({"status":"DB-ERROR", "message":"Error Selecting : %s " + err });
+			console.log("Error Selecting last draw: %s " + err );
+			result.lastDraw = lastDrawRow[0];
+			res.send(result);
+		});
+	});
+
 });
 
 //to prevent send a json to the users browser
