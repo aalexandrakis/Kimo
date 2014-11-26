94d55cd858846689025b9621dd0f897f83b3118d
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-27 00:42:00 +0200

    -- fix mysql configuration

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.static(path.join(__dirname, 'views')));
 
 app.use(
     connection(mysql,{
-        host: '127.3.55.130:3306',
+        host: '127.3.55.130',
         user: 'kimo',
         password : 'kimo',
         port : 3306, //port mysql
