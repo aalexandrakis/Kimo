553875e99ecc27251b96b1745618f55133b27790
aalexandrakis <aalexandrakis@hotmail.com>
2014-11-30 11:50:56 +0200

    -- remove a console.log

--- public/javascripts/general-functions.js
+++ public/javascripts/general-functions.js
@@ -1,6 +1,5 @@
     function fromIsoToEuro(dateString){
             var regExp = /(\d{4}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).(\d{2}).*/;
-            console.log(dateString);
             dateArray = regExp.exec(dateString);
             return dateArray[3] + "-" + dateArray[2] + "-" + dateArray[1] + " " + dateArray[4] + ":" + dateArray[5] + ":" + dateArray[6];
     }
