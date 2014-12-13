5f897bbc5f17f849a429d9859efc265f7130cd1b
aalexandrakis <aalexandrakis@hotmail.com>
2014-12-13 19:44:59 +0200

    -- fix instantiate a new mysql connection in paypalIPN script

--- routes/paypalIPN.js
+++ routes/paypalIPN.js
@@ -2,8 +2,7 @@ var express = require('express');
 var router = express.Router();
 var functions = require('../public/javascripts/functions.js')
 var qs = require('querystring');
-/* POST reset password. */
-
+//
 //router.get('/:txnId', function(req, res){
 //	data = {};
 //	data.body = {
@@ -14,9 +13,7 @@ var qs = require('querystring');
 //		mc_fee : '0.33',
 //		custom : '10'
 //	}
-//	req.getConnection(function(err, connection){
-//		checkAndUpdatePayment(connection, data);
-//	});
+//		checkAndUpdatePayment(data);
 //	res.status(200).send();
 //});
 
@@ -50,11 +47,7 @@ router.post('/', function(req, res) {
 		});
 		newRes.on('end', function (result) {
 			if(req.body.payment_status == "Completed"){
-				req.getConnection(function(err, connection){
-					if(err)
-						console.log("Could not complete payment with txn_id ", req.body.txn_id , " because of the following error.");
-					checkAndUpdatePayment(connection, req);
-				});
+					checkAndUpdatePayment(req);
 			}
 			console.log("response: ",response);
 		});
@@ -68,12 +61,24 @@ router.post('/', function(req, res) {
 });
 
 
-function checkAndUpdatePayment(connection, req){
+function checkAndUpdatePayment(req){
+
+	 var mysql = require('mysql');
+
+	//TODO this must be change connection must be declared only in one place
+	var connection = mysql.createConnection({
+		  host: process.env.OPENSHIFT_MYSQL_DB_HOST || 'localhost',
+		  user: process.env.MYSQL_USERNAME,
+		  password : process.env.MYSQL_PASSWORD,
+		  port : process.env.OPENSHIFT_MYSQL_DB_PORT || 3306, //port mysql
+		  database:'kimo'
+	});
+
 	amount = req.body.mc_gross - req.body.mc_fee;
 	query = "select * from payments where txnId = '" + req.body.txn_id + "' and userId = " + req.body.custom;
 	connection.query(query, function(err, result){
 		if (err)
-			console.log(err);
+			console.log("Could not complete payment with txn_id ", req.body.txn_id , " because of the following error:" , err);
 		if (result.length == 0){
 			values = {
 				payDateTime : req.body.payment_date,
