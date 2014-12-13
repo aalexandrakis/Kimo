7a141be6b602312b005b28ddbcc819a06978f03c
aalexandrakis <aalexandrakis@hotmail.com>
2014-12-13 04:20:02 +0200

    -- still trying to fix paypal ipn listener

--- routes/paypalInstantPaymentNotification.js
+++ routes/paypalInstantPaymentNotification.js
@@ -4,20 +4,26 @@ var functions = require('../public/javascripts/functions.js')
 /* POST reset password. */
 
 
-router.post('/', function(req, res) {
+router.get('/', function(req, res) {
 	res.status(200).send();
 //	req, res, url, data, dataCallBack, endCallBack
-	newReq = {
+
+	newReq1 = {
 		cmd : "_notify-validate"
 	};
-	newReq += req.body;
+
+	if (JSON.stringify(req.body == "{}")){
+		newReq = newReq1;
+	} else {
+		newReq = JSON.parse(JSON.stringify(newReq1)  + JSON.stringify(req.body));
+	}
 	response = "";
-	functions.externalHttpPost(null, null, newReq, "https://www.sandbox.paypal.com/cgi-bin/webscr",
+	functions.externalHttpPost(null, null, "https://www.sandbox.paypal.com/cgi-bin/webscr", JSON.stringify(newReq),
 
 	function(data){
 		response += data;
 	},
-	function(err, end){
+	function(err){
 		if (err){
 			console.log(err);
 		} else {
