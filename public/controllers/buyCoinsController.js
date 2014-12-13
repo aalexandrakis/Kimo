e60926f879414219bf4c5f5dd1f738dbf7344170
aalexandrakis <aalexandrakis@hotmail.com>
2014-12-13 02:47:47 +0200

    -- paypal button added

--- public/controllers/buyCoinsController.js
+++ public/controllers/buyCoinsController.js
@@ -54,6 +54,51 @@ kimoApp.controller("BuyCoinsController",  ['$scope', '$http', '$cookieStore', '$
           }
      };
 
+     $scope.paypalPayment = function(){
+          $scope.errorMessageGroup = {"display":"none"};
+          console.log("paypal payment");
+//          if(!check()){
+//              var password = "";
+//              $http({
+//                  url: '/addCoins',
+//                  method: "PUT",
+//                  headers: {Authorization: $cookieStore.get("user").token},
+//                  data: { 'userId': $cookieStore.get("user").userId ,'userName' : $scope.userName , userCoins: $scope.coins}
+//              })
+//              .then(function(response) {
+//                        if (response.data.message){
+//                            $scope.errorMessageGroup = {"display":"block"};
+//                            $scope.errorMessage = response.data.message;
+//                        } else {
+//                            user = $cookieStore.get("user")
+//                            user.userCoins = $scope.coins;
+//                            $cookieStore.put("user", user);
+//                            $rootScope.$broadcast('getUserInfo', {});
+//                            jSuccess(
+//                                 'Your coins updated successfully',
+//                                 {
+//                                   autoHide : true, // added in v2.0
+//                                   TimeShown : 3000,
+//                                   HorizontalPosition : 'right',
+//                                   VerticalPosition : 'top',
+//                                   onCompleted : function(){ // added in v2.0
+//                                     window.setTimeout(function(){
+//                                         window.location = '/#';
+//                                     }, 3000)
+//                                 }
+//                            });
+//                        }
+//
+//                  },
+//                  function(response) { // optional
+//                      // failed
+//                    $scope.errorMessageGroup = {"display":"block"};
+//                    $scope.errorMessage = "The request could not reach the server. Please try again later";
+//                  }
+//              );
+//          }
+     };
+
      check = function(){
         isError = false;
         if ($scope.coins == ""){
